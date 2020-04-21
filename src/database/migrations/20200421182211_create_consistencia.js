3b91b0f73c033c5a12c3eee111afcb613fa591cd
kevyn <kevynfariaga@gmail.com>
2020-04-21 18:47:04 -0300

    Criação do banco de dados com users e consistencia

---
+++ src/database/migrations/20200421182211_create_consistencia.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('consistencia', function (table) {
+      table.increments();
+      table.string('dia_semana').notNullable();
+      table.string('dia_anterior').notNullable();
+      table.string('description').notNullable();
+      table.string('value').notNullable();
+      table.string('id_usuario').notNullable();
+      table.foreign('id_usuario').references('id').inTable('users');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('consistencia');
+};
