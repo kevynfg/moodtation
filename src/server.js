6971e30c3fd644b4316411b8b1fd1e8ba85a3eae
Kevyn <kevynredlotus@gmail.com>
2020-05-13 23:17:12 -0300

    Adicionado Cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const routes = require('./routes'); //importar o routes do routes.js
 
+const cors = require('cors');
+
+
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use(routes); //serve para quando quer colocar algum tipo de configuração que está em outro arquivo, ou colocar algum módulo
 
