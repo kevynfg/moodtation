1153dadf7ef5296b39d8d19d691b91fd2f34f55d
kevyn <kevynredlotus@gmail.com>
2020-09-03 21:03:41 -0300

    - Cookies - Bcrypt - Login e Cadastro

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,66 +1,66 @@
-const knex = require("../database")
+const knex = require('../database');
 // const update = require("lodash.update");
 
 module.exports = {
-    async index(req, res, next) {
-        try {
-            const results = await knex('users')
-            .where('deleted_at', null)
+  async index(req, res, next) {
+    try {
+      const results = await knex('users').where('deleted_at', null);
 
-            return res.json(results);
-        } catch (error) {
-            next(error)
-        }
-        
-    },
-    //como terceiro parâmetro coloco o next que lida com o erro de request
-    //vindo do server.js
-    async create(req, res, next) {
-        
-        try {
-            const {username} = req.body;
-
-            await knex('users').insert({
-                username
-            })
-
-            return res.status(201).send()
-
-        } catch (error) {
-            next(error)
-        }
-    },
-    async update(req, res, next) {
-        try {
-            //pega dados do corpo em json
-            const { username } = req.body
-            //pegar id do parâmetro /users/:id
-            const { id } = req.params
-
-            await knex('users').update({ username }).where({ id })
-
-            return res.send()
-
-        } catch (error) {
-            next(error)
-        }
-    },
-    async delete(req, res, next) {
-        try {
-            const { id } = req.params
-
-            await knex('users')
-            .where({ id })
-            .update('deleted_at', new Date())
-            
-
-            return res.send()
-
-        } catch (error) {
-            next(error)
-        }
-    },
-    pegarEmail: function (email) {
-        return knex('users').where('email', email).first();
+      return res.json(results);
+    } catch (error) {
+      next(error);
     }
-}
+  },
+  //como terceiro parâmetro coloco o next que lida com o erro de request
+  //vindo do server.js
+  async create(req, res, next) {
+    try {
+      const { username } = req.body;
+
+      await knex('users').insert({
+        username,
+      });
+
+      return res.status(201).send();
+    } catch (error) {
+      next(error);
+    }
+  },
+  async update(req, res, next) {
+    try {
+      //pega dados do corpo em json
+      const { username } = req.body;
+      //pegar id do parâmetro /users/:id
+      const { id } = req.params;
+
+      await knex('users').update({ username }).where({ id });
+
+      return res.send();
+    } catch (error) {
+      next(error);
+    }
+  },
+  async delete(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      await knex('users').where({ id }).update('deleted_at', new Date());
+
+      return res.send();
+    } catch (error) {
+      next(error);
+    }
+  },
+  pegarEmail: function (email) {
+    return knex('users').where('email', email).first();
+  },
+  create: function (user) {
+    //insere um usuário no banco e pega de volta um array de usuários
+    //pega o primeiro ID deste array
+    return knex('users')
+      .insert(user, 'id')
+      .then((ids) => {
+        return ids[0]; //
+      });
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,29 +1,33 @@
 const express = require('express');
 const routes = require('./routes'); //importar o routes do routes.js
 const auth = require('./auth');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 const server = express();
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(cookieParser());
 
 //esta linha faz com que o server passe a usar requests em json
 server.use(express.json());
 server.use(routes);
 server.use('/auth', auth);
 
-
 //rota de error para rotas que não são achadas
 server.use((req, res, next) => {
-    const error = new Error('Not found')
-    error.status = 404
-    next(error)
-})
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
 
 //capturar os erros do server, um middleware
 //next é o próximo passo da requisição
 server.use((error, req, res, next) => {
-    //verifica o erro, código de erro do HTTP é entre 500-599
-    res.status(error.status || 500)
-    res.json({ error: error.message })
-})
-
+  //verifica o erro, código de erro do HTTP é entre 500-599
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+});
 
 server.listen(3333, () => console.log('Servidor rodando...'));
